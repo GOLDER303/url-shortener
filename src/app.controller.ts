e16fcfcae324a50c9ea46e79a9beca27f15a9e8d
GOLDER303 <szymonstepien@interia.pl>
2023-09-09 17:55:55 +0200

    Add url shortening logic

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,20 @@
-import { Controller, Get, Render } from '@nestjs/common';
+import { Body, Controller, Get, Post, Redirect, Render } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('/')
   @Render('index')
-  getHello() {
-    return { message: 'Hello World!' };
+  async getIndex() {
+    const urls = await this.appService.getAllURLs();
+    return { urls };
+  }
+
+  @Post('/shortURLs')
+  @Redirect('/')
+  async createShortURL(@Body('fullURL') fullURL: string) {
+    await this.appService.createShortURL(fullURL);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { Url } from '@prisma/client';
+import { nanoid } from 'nanoid';
+import { PrismaService } from './prisma/prisma.service';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private prisma: PrismaService) {}
+
+  async getAllURLs(): Promise<Url[]> {
+    return await this.prisma.url.findMany();
+  }
+
+  async createShortURL(fullURL: string): Promise<Url> {
+    return await this.prisma.url.create({
+      data: {
+        fullURL,
+        shortURL: nanoid(6),
+      },
+    });
   }
 }
